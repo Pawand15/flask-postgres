a4bbde18cc9814cf9647fa44b7aaafa3228f8233
Pawan Deora <pawan.d@outlook.com>
2022-05-23 12:59:29 +0530

    Remove DB URI

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-os.environ['DATABASE_URI'] = "postgresql://postgres:postgres@10.38.6.42:5432/ntnx"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
 db = SQLAlchemy(app)
